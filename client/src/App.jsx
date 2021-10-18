3ec40eca4e6dfee57f88f2ca0b902e614566a88d
cmccurd <cam.alex.mccurdy@gmail.com>
2021-10-18 19:49:56 -0400

    serve static html file for default path.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {}
   }
-
+  componentDidMount() {
+    console.log('hey')
+  };
 
 
   render() {
--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 const get = require('./modules/get.js');
 const post = require('./modules/post.js')
+const path = require('path');
 
 const express = require('express');
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
-app.use(express.static('./public'));
+app.use('/', express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
 
+
 app.get('/api/cows', (req, res) => {
-  // res.send([{name: 'Buttercup', description: '...'}, {name: 'Daisy', description: '...'}]);
   get().then((val) => {
     console.log(val);
     res.send(val);
